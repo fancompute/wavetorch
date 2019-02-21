edb305f5e4e5384fdb22d869fa9f4d861af4dd03
Ian Williamson <iwill@stanford.edu>
2019-02-21 14:15:23 -0800

    Added batching and data loading

--- wavetorch/data.py
+++ wavetorch/data.py
@@ -1,18 +1,39 @@
 import torch
 import librosa
 import numpy as np
+import os
 
-PATH = 'data/vowels'
-
-def load_vowel(prefix, sr=None, normalize=True):
-    data, rate = librosa.load('{}/{}.wav'.format(PATH, prefix), sr=sr)
-
-    if normalize:
-        data = normalize_vowel(data)
-
-    return torch.tensor(data), rate
 
 def normalize_vowel(wav_data):
     total_power = np.square(wav_data).sum()
 
     return wav_data / np.sqrt(total_power)
+
+
+def load_vowel(file, sr=None, normalize=True):
+    data, rate = librosa.load(file, sr=sr)
+
+    if normalize:
+        data = normalize_vowel(data)
+
+    return torch.tensor(data)
+
+
+def load_all_vowels(directories_str, sr=None, normalize=True):
+    inputs = []
+    labels = []
+    for i, directory_str in enumerate(directories_str):
+        directory = os.fsencode(directory_str)
+        label = torch.eye(len(directories_str))[i]
+
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".wav"):
+                input = load_vowel(os.path.join(directory_str, filename), sr=sr, normalize=normalize)
+                inputs.append(input)
+                labels.append(label)
+                continue
+            else:
+                continue
+
+    return torch.nn.utils.rnn.pad_sequence(inputs, batch_first=True), torch.nn.utils.rnn.pad_sequence(labels, batch_first=True)
--- wavetorch/wave.py
+++ wavetorch/wave.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class WaveCell(torch.nn.Module):
 
-    def __init__(self, Nt, dt, Nx, Ny, h, mask_src, mask_probe, c2=None, pml_N=20, pml_p=3.0, pml_max=0.5):
+    def __init__(self, dt, Nx, Ny, h, src_x, src_y, c2=None, pml_N=20, pml_p=3.0, pml_max=0.5):
         super(WaveCell, self).__init__()
 
         self.dt = dt
@@ -17,30 +17,30 @@ class WaveCell(torch.nn.Module):
         self.Ny = Ny
         self.h  = h
 
+        self.src_x = src_x
+        self.src_y = src_y
+
         if c2 is None:
             c2 = torch.ones(Nx, Ny, requires_grad=True)
         self.c2  = torch.nn.Parameter(c2)
 
-        self.register_buffer("mask_src", mask_src)
-        self.register_buffer("mask_probe", mask_probe)
-
         # Setup the PML/adiabatic absorber
         b_vals = pml_max * torch.linspace(0.0, 1.0, pml_N+1) ** pml_p
 
-        b_x = torch.zeros(Nx, Ny, requires_grad=False).unsqueeze(0).unsqueeze(0)
-        b_x[0,0,0:pml_N+1,:]   = torch.flip(b_vals, [0]).repeat(Ny,1).transpose(0, 1)
-        b_x[0,0,-pml_N-2:-1,:] = b_vals.repeat(Ny,1).transpose(0, 1)
+        b_x = torch.zeros(Nx, Ny, requires_grad=False)
+        b_x[0:pml_N+1,   :] = torch.flip(b_vals, [0]).repeat(Ny,1).transpose(0, 1)
+        b_x[-pml_N-2:-1, :] = b_vals.repeat(Ny,1).transpose(0, 1)
 
-        b_y = torch.zeros(Nx, Ny, requires_grad=False).unsqueeze(0).unsqueeze(0)
-        b_y[0,0,:,0:pml_N+1]   = torch.flip(b_vals, [0]).repeat(Nx,1)
-        b_y[0,0,:,-pml_N-2:-1] = b_vals.repeat(Nx,1)
+        b_y = torch.zeros(Nx, Ny, requires_grad=False)
+        b_y[:,   0:pml_N+1] = torch.flip(b_vals, [0]).repeat(Nx,1)
+        b_y[:, -pml_N-2:-1] = b_vals.repeat(Nx,1)
 
         self.register_buffer("b", torch.sqrt( b_x**2 + b_y**2 ))
 
         # Define the finite differencing coeffs for convenience
-        self.register_buffer("C1", (self.dt**(-2) + self.b * 0.5 * self.dt**(-1)).pow(-1))
-        self.register_buffer("C2", torch.tensor(2 * self.dt**(-2)))
-        self.register_buffer("C3", self.dt**(-2) - self.b * 0.5 * self.dt**(-1))
+        self.register_buffer("A1", (self.dt**(-2) + self.b * 0.5 * self.dt**(-1)).pow(-1))
+        self.register_buffer("A2", torch.tensor(2 * self.dt**(-2)))
+        self.register_buffer("A3", self.dt**(-2) - self.b * 0.5 * self.dt**(-1))
 
         # Define the laplacian conv kernel
         self.register_buffer("laplacian", h**(-2) * torch.tensor([[[[0.0,  1.0, 0.0],
@@ -49,46 +49,39 @@ class WaveCell(torch.nn.Module):
                                                                     requires_grad=False))
 
     def step(self, F, un1, un2):
-        un = self.C1 * ( self.C2 * un1 - self.C3 * un2 + self.c2 * conv2d(un1, self.laplacian, padding=1) + self.mask_src * F )
+        un = torch.mul( self.A1, ( torch.mul(self.A2, un1) 
+                                   - torch.mul(self.A3, un2) 
+                                   + torch.mul( self.c2, conv2d(un1.unsqueeze(1), self.laplacian, padding=1).squeeze(1) ) ))
+        un[:, self.src_x, self.src_y] = un[:, self.src_x, self.src_y] + F.squeeze(1)
         return un, un, un1
 
-    def forward(self, input, hidden, loss_func=None):
-        un1, un2 = hidden
+    def forward(self, x):
+        batch_size = x.shape[0]
+        un1 = torch.zeros(batch_size, self.Nx, self.Ny)
+        un2 = torch.zeros(batch_size, self.Nx, self.Ny)
+        un_all = []
 
-        loss = 0.0
-        for xi in input.split(1):
+        for i, xi in enumerate(x.chunk(x.size(1), dim=1)):
             un, un1, un2 = self.step(xi, un1, un2)
+            un_all.append(un)
 
-            if loss_func is not None:
-                loss += loss_func(un)
+        un = torch.stack(un_all, dim=1)
 
-        return un, loss
+        return un
 
-    def animate(self, x, block=True):
+    def animate(self, x, block=True, batch_ind=0):
         fig, ax = plt.subplots()
 
-        un_max = 0.0
-        uns = []
         ims = []
-
         with torch.no_grad():
-            un1 = torch.zeros(self.Nx, self.Ny).unsqueeze(0).unsqueeze(0)
-            un2 = torch.zeros(self.Nx, self.Ny).unsqueeze(0).unsqueeze(0)
-            for xi in x.split(1):
-                un, un1, un2 = self.step(xi, un1, un2)
-                tmp = un.detach().numpy().squeeze().transpose()
-                tmp_max = np.abs(tmp).max()
-                if tmp_max > un_max:
-                    un_max = tmp_max
+            y = self.forward(x[batch_ind].unsqueeze(0))
 
-                uns.append(tmp)
-
-        for i in range(0, len(uns)):
-            im = plt.imshow(uns[i], cmap=plt.cm.RdBu, animated=True, vmin=-un_max, vmax=+un_max)
+        y_max = torch.max(y).item()
+        for i in range(0, y.shape[1]):
+            im = plt.imshow(y[0,i,:,:].numpy().transpose(), cmap=plt.cm.RdBu, animated=True, vmin=-y_max, vmax=+y_max, origin="bottom")
             ims.append([im])
 
         ani = animation.ArtistAnimation(fig, ims, interval=1, blit=True, repeat_delay=1000)
-
         plt.show(block=block)
 
         return ani
