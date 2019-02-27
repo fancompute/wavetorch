31851d7e3d95667cbe9c92c9d312a1e2a275dfc6
Ian Williamson <iwill@stanford.edu>
2019-02-26 19:06:38 -0800

    Added comments and fixed WaveCell signature

--- study/train.py
+++ study/train.py
@@ -10,7 +10,6 @@ from wavetorch import *
 
 import argparse
 import time
-import os
 
 if __name__ == '__main__':
     # Parse command line arguments
@@ -36,6 +35,7 @@ if __name__ == '__main__':
 
     torch.set_num_threads(args.num_threads)
 
+    # figure out which device we're on
     if args.use_cuda and torch.cuda.is_available():
         print("Using GPU...")
         args.dev = torch.device('cuda')
@@ -43,8 +43,7 @@ if __name__ == '__main__':
         print("Using CPU...")
         args.dev = torch.device('cpu')
 
-    h  = args.dt * 2.01 / 1.0
-
+    # Each dir corresponds to a distinct class and is automatically given a corresponding one hot
     directories_str = ("./data/vowels/a/",
                        "./data/vowels/e/",
                        "./data/vowels/o/")
@@ -56,26 +55,27 @@ if __name__ == '__main__':
     x = x.to(args.dev)
     y_labels = y_labels.to(args.dev)
 
+    # Put tensors into Datasets and then Dataloaders to let pytorch manage batching
     full_ds = TensorDataset(x, y_labels)
     train_ds, test_ds = random_split(full_ds, [int(args.ratio_train*N_samples), N_samples-int(args.ratio_train*N_samples)])
 
     train_dl = DataLoader(train_ds, batch_size=args.batch_size, shuffle=True)
     test_dl = DataLoader(test_ds, batch_size=args.batch_size)
 
-    # --- Setup probe coords and loss func
+    # Setup probe coords and loss func
     probe_x = args.probe_x
     probe_y = torch.arange(args.probe_y, args.probe_y + N_classes*args.probe_space, args.probe_space)
 
     criterion = torch.nn.CrossEntropyLoss()
 
-    # --- Define model
-    model = WaveCell(args.dt, args.Nx, args.Ny, h, args.src_x, args.src_y, probe_x, probe_y, pml_max=3, pml_p=4.0, pml_N=20)
+    # Define model
+    model = WaveCell(args.dt, args.Nx, args.Ny, args.src_x, args.src_y, probe_x, probe_y, pml_max=3, pml_p=4.0, pml_N=20)
     model.to(args.dev)
 
-    # --- Define optimizer
+    # Define optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
-    # --- Run training
+    # Run training
     print("Using a sample rate of %d Hz (sequence length of %d)" % (args.sr, x.shape[1]))
     print("Using %d total samples (%d of each vowel)" % (len(train_ds)+len(test_ds), args.num_of_each) )
     print("   %d for training" % len(train_ds))
@@ -96,6 +96,8 @@ if __name__ == '__main__':
         train_acc_ep = []
 
         for xb, yb in train_dl:
+            # Needed to define this for LBFGS.
+            # Technically, Adam doesn't require this but we can be flexible this way
             def closure():
                 optimizer.zero_grad()
                 loss = criterion(model(xb), yb.argmax(dim=1))
@@ -115,6 +117,7 @@ if __name__ == '__main__':
             for xb, yb in test_dl:
                 test_acc_ep.append( accuracy(model(xb), yb.argmax(dim=1)) )
 
+        # Log metrics
         hist_loss_batches.append(np.mean(loss_batches_ep))
         hist_test_acc.append(np.mean(test_acc_ep))
         hist_train_acc.append(np.mean(train_acc_ep))
@@ -122,12 +125,13 @@ if __name__ == '__main__':
         print('Epoch: %2d/%2d   %4.1f sec   |   L = %.3e   accuracy = %.4f (train) / %.4f (test)' % 
                 (epoch, args.N_epochs, time.time()-t_epoch, hist_loss_batches[-1], hist_train_acc[-1], hist_test_acc[-1]))
 
+    # Finished training
     print(" --- ")
     print('Total time: %.1f min' % ((time.time()-t_start)/60))
     
     save_model(model)
     
-    # Get CM
+    # Calculate and print confusion matrix
     cm_train, cm_test = calc_cm(model, train_dl, test_dl)
 
     print("Training CM")
