431415b860dd4a444b7439b177773d6e97d7c13e
Ian Williamson <ian.williamson@ieee.org>
2019-09-20 10:59:40 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='wavetorch',
+    version='0.1.x',
+    description='Numerically solving and backpropagating through the wave equation using pytorch',
+    url='https://github.com/fancompute/wavetorch',
+    author='Ian Williamson, Tyler Hughes, Momchil Minkov, Shanhui Fan',
+    author_email='ian.williamson@ieee.org',
+    license='MIT',
+    packages=['wavetorch'],
+    scripts=[],
+    zip_safe=False)
